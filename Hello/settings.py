e78ad53a738973d4637d6cbc9f5cb8bdbc6033d7
7Loaded <48351115+DhruvalBhinsara1@users.noreply.github.com>
2020-12-10 02:01:07 +0530

    edit

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_uirl#8%yg1^uh%e+6@st*tiawphx_s%6ye$b1^^_j$4iuq$bc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','dhruval-portfolio.herokuapp.com']
 
 
 # Application definition

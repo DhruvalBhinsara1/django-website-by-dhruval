a8091f2bac220f41799fb6bfa5dbcbe3d2e86da2
7Loaded <48351115+DhruvalBhinsara1@users.noreply.github.com>
2020-12-10 01:13:45 +0530

    Commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_uirl#8%yg1^uh%e+6@st*tiawphx_s%6ye$b1^^_j$4iuq$bc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','dhruvalbhinsara.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,4 +126,3 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- Hello/urls.py
+++ Hello/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import serve
-from django.conf.urls import url
-from django.conf import settings
 
 admin.site.site_header = "7Loaded-Projects Admin"
 admin.site.site_title = "7Loaded-Projects Admin Portal"
@@ -25,7 +22,5 @@ admin.site.index_title = "Welcome to 7Loaded-Projects Researcher Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
-    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}), 
+    path('',include('home.urls'))
 ]

f1507c7ebb4115f9bbabd696d5e9e9384c337bc1
7Loaded <48351115+DhruvalBhinsara1@users.noreply.github.com>
2020-12-10 10:02:04 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+#this a text
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
